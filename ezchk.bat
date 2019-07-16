c8c7e66aa14bc4727a9aba2993b9d33cd855945f
Chawanat Nakasan <chawanat.n@gmail.com>
2019-07-16 09:48:07 +0900

    basic functionality

---
+++ ezchk.bat
@@ -0,0 +1,32 @@
+@echo off
+
+set FILENAME= "log.txt"
+
+echo # ... ezchk Vital Information Diagnostics ... > %FILENAME%
+
+echo ## Hardware >> %FILENAME%
+
+echo: >> %FILENAME%
+echo ### WMIC baseboard >> %FILENAME%
+wmic baseboard get manufacturer,model | find /v "" >> %FILENAME%
+
+echo: >> %FILENAME%
+echo ### WMIC CPU >> %FILENAME%
+wmic cpu get manufacturer,name,datawidth,numberofcores,numberofenabledcore,threadcount | find /v "" >> %FILENAME%
+
+echo: >> %FILENAME%
+echo ### WMIC NIC >> %FILENAME%
+wmic nic get adaptertype,manufacturer,name,index,macaddress | find /v "" >> %FILENAME%
+
+echo: >> %FILENAME%
+echo ### ipconfig /all >> %FILENAME%
+ipconfig /all >> %FILENAME%
+
+echo: >> %FILENAME%
+echo ## Network Functions >> %FILENAME%
+
+echo: >> %FILENAME%
+echo ### DNS >> %FILENAME%
+nslookup example.com 2>nul >> %FILENAME%
+
+
