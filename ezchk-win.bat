af1e5912e2c462799bc8fd77a43d4cdbdc63970d
Chawanat Nakasan <chawanat.n@gmail.com>
2019-07-25 12:04:45 +0900

    Add DNS record counter

--- ezchk-win.bat
+++ ezchk-win.bat
@@ -9,47 +9,51 @@ echo # ... ezchk Vital Information Diagnostics ... > %FILENAME%
 
 echo ## Hardware >> %FILENAME%
 
-echo: >> %FILENAME%
+echo\ >> %FILENAME%
 echo ### WMIC baseboard >> %FILENAME%
 wmic baseboard get manufacturer,model | find /v "" >> %FILENAME%
 
-echo: >> %FILENAME%
+echo\ >> %FILENAME%
 echo ### WMIC CPU >> %FILENAME%
 wmic cpu get manufacturer,name,datawidth,numberofcores,numberofenabledcore,threadcount | find /v "" >> %FILENAME%
 
-echo: >> %FILENAME%
+echo\ >> %FILENAME%
 echo ### WMIC NIC >> %FILENAME%
 wmic nic get adaptertype,manufacturer,name,index,macaddress | find /v "" >> %FILENAME%
 
-echo: >> %FILENAME%
+echo\ >> %FILENAME%
 echo ### ipconfig /all >> %FILENAME%
 ipconfig /all >> %FILENAME%
 
-echo: >> %FILENAME%
+echo\ >> %FILENAME%
 echo ### netsh interface show interface >> %FILENAME%
 netsh interface show interface >> %FILENAME%
 
-echo: >> %FILENAME%
+echo\ >> %FILENAME%
 echo ### netsh wlan show interface >> %FILENAME%
 netsh wlan show interface >> %FILENAME%
 
-echo: >> %FILENAME%
+echo\ >> %FILENAME%
 echo ### POWERSHELL: Wireless Modes >> %FILENAME%
 powershell.exe Get-NetAdapteradvancedproperty "Wi-Fi*" | find /I "mode" >> %FILENAME%
 
-echo: >> %FILENAME%
+echo\ >> %FILENAME%
+echo ### POWERSHELL: Number of Meaningful Lines in etc/hosts file >> %FILENAME%
+powershell.exe -Command "Get-Content $Env:WINDIR\System32\drivers\etc\hosts | Select-String -Pattern '(^(\s*#))' -NotMatch | select -exp line | measure-object -Line | Out-String | Select-String -pattern '\d' -allmatches | Foreach-Object { $_.Matches} | Foreach-Object {$_.value}" >> %FILENAME%
+
+echo\ >> %FILENAME%
 echo ## Network Functions >> %FILENAME%
 
-echo: >> %FILENAME%
-echo ### DNS >> %FILENAME%
+echo\ >> %FILENAME%
+echo ### DNS (Default) >> %FILENAME%
 nslookup example.com 2>nul >> %FILENAME%
 
-echo: >> %FILENAME%
-echo ### HTTP (True == Works) >> %FILENAME%
+echo\ >> %FILENAME%
+echo ### POWERSHELL: HTTP (True == Works) >> %FILENAME%
 powershell -Command "Invoke-Webrequest http://example.com/ >$null 2>$null ; $?" >> %FILENAME%
 
-echo: >> %FILENAME%
-echo ### HTTPS (True == Works) >> %FILENAME%
+echo\ >> %FILENAME%
+echo ### POWERSHELL: HTTPS (True == Works) >> %FILENAME%
 powershell -Command "Invoke-Webrequest https://example.com/ >$null 2>$null ; $?" >> %FILENAME%
 
 :: TODO: Pass 2 should find and redact MAC addresses
